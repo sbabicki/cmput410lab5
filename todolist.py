d2f28e25568fba34c41c2b16920074649bf6ea01
Sasha Babicki <sbabicki@gmail.com>
2015-02-11 19:26:24 -0700

    login works

--- todolist.py
+++ todolist.py
@@ -9,13 +9,14 @@ import sqlite3
 
 DATABASE = 'todo.db'
 DEBUG = True
-SECRET_KEY = 'development key'
-USERNAME = 'admin'
-PASSWORD = 'default'
-
-conn = None
+SECRET_KEY = 'blah blah blah'
+USERNAME = 'a'
+PASSWORD = 'b'
 
 app = Flask(__name__)
+app.config.from_object(__name__)
+
+conn = None
 
 def get_conn():
     #tells interpreter to use conn defined above
@@ -47,10 +48,34 @@ def add_task(category, priority, description):
     #needed for saving in database
     get_conn().commit()
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-	error = "test"
-	return render_template('login.html', errors=error)
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+	
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+    if not session.get('logged_in'):
+    	abort(401)
+	g.db.execute('insert into entries (title, text) values (?, ?)',[request.form['title'], request.form['text']])
+    g.db.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('show_entries'))
 
 @app.route('/')
 @app.route('/show_entries')
